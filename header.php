a57c52b9a7428780255ffde03f55b68dbed925d3
Alejandro Pérez <aleperez92@gmail.com>
2013-10-21 23:24:48 +0100

    Added blank lines for readability

--- header.php
+++ header.php
@@ -7,8 +7,11 @@
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+    
 <head>
+    
     <title><?php base_title(); ?></title>
+    
     <!-- Meta -->
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -16,17 +19,22 @@
     <meta name="copyright" content="<?php echo date('Y') ?>">
     <meta name="description" content="<?php bloginfo( 'description' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <!-- Google+ Profile -->
     <link rel="author" href="https://plus.google.com/1234...567/">
+    
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.png" sizes="16x16" />
     <link rel="icon" type="image/x-icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.png" sizes="16x16" />
     <link rel="apple-touch-icon-precomposed" href="<?php bloginfo('template_directory'); ?>/images/apple-favicon.png" />
+    
     <!-- Stylesheet -->
     <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    
     <!-- Wordpress generated code -->
     <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
     <?php wp_head(); ?>
+    
 </head>
 
 <body>
